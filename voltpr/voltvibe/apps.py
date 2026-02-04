b6b0a1a7e8ebde68924de3be5732a212f3878085
millenium-1 <louiskaroki2010@gmail.com>
2026-02-04 10:32:41 +0300

    creating signals to automatically add customers and profile

--- voltpr/voltvibe/apps.py
+++ voltpr/voltvibe/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 
-
 class VoltvibeConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'voltvibe'
+
+    def ready(self):
+        import voltvibe.signals
---
+++ voltpr/voltvibe/signals.py
@@ -0,0 +1,13 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+from .models import Profile, Customer
+
+@receiver(post_save, sender=User)
+def create_user_related_objects(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+        Customer.objects.create(
+            user=instance,
+            email=instance.email
+            )
