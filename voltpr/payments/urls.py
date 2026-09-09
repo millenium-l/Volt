c4fa6ab45cb71678f5b756429abd42f4f28d7109
millenium-1 <louiskaroki2010@gmail.com>
2026-09-09 14:30:47 +0300

    added urls in payment app

--- voltpr/payments/urls.py
+++ voltpr/payments/urls.py
@@ -1,12 +1,8 @@
 from django.urls import path
 
-from .views import initiate_payment, mpesa_callback, payment_page
+from .views import stk_push
 
-
-app_name = 'payments'
 
 urlpatterns = [
-    path('<int:order_id>/', payment_page, name='payment_page'),
-    path('initiate/<int:order_id>/', initiate_payment, name='initiate'),
-    path('mpesa/callback/', mpesa_callback, name='mpesa_callback'),
+    path("stk-push/",stk_push,name="stk_push"),
 ]
