95c8f8b37816fe772aacad0f27b3ca4b44fa7c11
millenium-1 <louiskaroki2010@gmail.com>
2026-09-09 14:55:15 +0300

    added mpesa callback url

--- voltpr/payments/urls.py
+++ voltpr/payments/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import stk_push
+from .views import stk_push, mpesa_callback
 
 
 urlpatterns = [
-    path("stk-push/",stk_push,name="stk_push"),
+    path("stk-push/",stk_push,name="stk_push",),
+    path("callback/",mpesa_callback,name="mpesa_callback",),
 ]
